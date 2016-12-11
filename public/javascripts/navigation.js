8c76346becdadf1962b939248a7bc65691fb1b01
Jarod Adair <jarod.adair@manheim.com>
2016-12-10 22:36:03 -0600

    adding some more image stuff

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,21 +1,8 @@
 $(document).ready(function(){
-  $("#add").click(function(){
-    $("#add-modal").show();
-  });
 
   $('.handle').click(function() {
     $('body').toggleClass('slide');
   });
 
-  console.log('boom');
-  var mySwiper = new Swiper ('.swiper-container', {
-    scrollbar: '.swiper-scrollbar',
-    scrollbarHide: true,
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    spaceBetween: 30,
-    grabCursor: true
-  });
-  console.log(mySwiper);
 });
 
