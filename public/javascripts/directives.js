ef2c38b257a6f65d16837cecaf2e7150582d38e9
Jarod Adair <jarod.adair@manheim.com>
2017-01-07 17:10:09 -0600

    moving list items to directives

--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -1,14 +1,58 @@
+function showDelete (element) {
+  $(element).addClass('show-delete');
+}
+
+function hideDelete (element) {
+  $(element).removeClass('show-delete');
+}
+
 angular.module('tacoTruck')
 
-  .directive('test', function(){
-
+  .directive('locationsListItem', function(){
     return {
       restrict: 'E',
-      templateUrl: 'location-item.html',
+      templateUrl: 'templates/locations-list-item.html',
       link: function(scope, element) {
-        element.parent().on('swipeleft', function() {$(this).addClass('show-delete')});
-        element.parent().on('swiperight', function() {$(this).removeClass('show-delete')});
-        element.parent().on('click', function() {$(this).removeClass('show-delete')});
+        element.parent().on('swipeleft', function() { showDelete(this) });
+        element.parent().on('swiperight', function() { hideDelete(this) });
+        element.parent().on('click', function() { hideDelete(this) });
+      }
+    }
+  })
+
+  .directive('itemsListItem', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/items-list-item.html',
+      link: function(scope, element) {
+        element.parent().on('swipeleft', function() { showDelete(this) });
+        element.parent().on('swiperight', function() { hideDelete(this) });
+        element.parent().on('click', function() { hideDelete(this) });
+      }
+    }
+  })
+
+  .directive('reviewsListItem', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/reviews-list-item.html',
+      link: function(scope, element) {
+        element.parent().on('swipeleft', function() { showDelete(this) });
+        element.parent().on('swiperight', function() { hideDelete(this) });
+        element.parent().on('click', function() { hideDelete(this) });
+      }
+    }
+  })
+
+  .directive('imageSlider', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/image-slider.html',
+      link: function(scope, element) {
+        var swiper = new Swiper('.swiper-container', {
+          slidesPerView: 3,
+          slidesPerColumnFill: 'column'
+        });
       }
     }
   });
