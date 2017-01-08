e0c8e09a09e25ac21a52d80b919ea907853c5ed6
Jarod Adair <jarod.adair@manheim.com>
2017-01-07 18:18:10 -0600

    moving routing information back :/

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,24 +1,2 @@
 angular.module('tacoTruck')
-.constant('tacoTruckApiUrl', "http://localhost:3000/api/v1")
-
-.config(function($routeProvider, $locationProvider, cloudinaryProvider) {
-  cloudinaryProvider
-    .set("cloud_name", "dfavubcrx")
-    .set("upload_preset", "xdi4oxap");
-
-  $routeProvider
-    .when('/', {
-      templateUrl: '/pages/location.html',
-      controller: 'locationController'
-    })
-    .when('/location/:location_id', {
-      templateUrl: '/pages/item.html',
-      controller: 'itemController'
-    })
-    .when('/location/:location_id/item/:item_id', {
-      templateUrl: '/pages/review.html',
-      controller: 'reviewController'
-    })
-    .otherwise({redirectTo: '/blah'});
-  $locationProvider.html5Mode(true);
-});
+.constant('tacoTruckApiUrl', "http://localhost:3000/api/v1");
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -269,4 +269,26 @@ angular.module('tacoTruck', ['ngRoute', 'FacebookProvider', 'photoAlbumControlle
       $rootScope.$apply();
     });
 
+  })
+
+  .config(function($routeProvider, $locationProvider, cloudinaryProvider) {
+    cloudinaryProvider
+      .set("cloud_name", "dfavubcrx")
+      .set("upload_preset", "xdi4oxap");
+
+    $routeProvider
+      .when('/', {
+        templateUrl: '/pages/location.html',
+        controller: 'locationController'
+      })
+      .when('/location/:location_id', {
+        templateUrl: '/pages/item.html',
+        controller: 'itemController'
+      })
+      .when('/location/:location_id/item/:item_id', {
+        templateUrl: '/pages/review.html',
+        controller: 'reviewController'
+      })
+      .otherwise({redirectTo: '/blah'});
+    $locationProvider.html5Mode(true);
   });
