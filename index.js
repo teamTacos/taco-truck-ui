a5c44be25812541ec94e52ca77e5d019a4ee339d
Jarod Adair <jarod.adair@manheim.com>
2017-02-24 14:26:35 -0600

    changing js load paths

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.set('view engine', 'ejs');
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
+app.use('/angular', express.static(__dirname + '/node_modules/angular'));
+app.use('/angular-route', express.static(__dirname + '/node_modules/angular-route'));
+app.use('/cloudinary-core', express.static(__dirname + '/bower_components/cloudinary-core'));
+app.use('/cloudinary-jquery-file-upload', express.static(__dirname + '/node_modules/cloudinary-jquery-file-upload'));
+app.use('/cloudinary-ng', express.static(__dirname + '/bower_components/cloudinary_ng/js'));
+app.use('/lodash', express.static(__dirname + '/bower_components/lodash'));
+app.use('/ng-file-upload', express.static(__dirname + '/bower_components/ng-file-upload'));
+app.use('/swiper', express.static(__dirname + '/node_modules/swiper/dist'));
+
 app.use('/*', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
